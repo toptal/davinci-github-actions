e8e2d41aeb3ad69485d0092746547de66b4fafe3
Ondřej Tůma <ondra.tuma@toptal.com>
2022-09-07 15:54:36 +0200

    chore: added changeset lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['davinci-syntax lint code', 'prettier --write'],
+  '{**/*.{js,jsx,ts,tsx},.changeset/*.md}': ['davinci-syntax lint code', 'prettier --write'],
   '{action.yml,README.md}': paths =>
     paths.length > 0
       ? ['yarn documentation:generate', `git add */README.md`]
