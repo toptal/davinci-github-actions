1f15f014bd87a9debb4a1ef8b05f473682e51c65
Daniel O <daniel.ostapenko@toptal.com>
2022-10-28 15:00:25 +0200

    feat: add dependency cruiser metrics report action (#59)

---
+++ .changeset/cuddly-rats-report.md
@@ -0,0 +1,9 @@
+---
+'davinci-github-actions': minor
+---
+
+---
+
+### depcuiser-metrics-report
+
+- add an action to report dependency cruiser metrics
---
+++ depcuiser-metrics-report/dependency-cruiser-metrics-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  options: {
+    tsPreCompilationDeps: true,
+    tsConfig: {
+      fileName: 'tsconfig.dependency-cruiser.json',
+    },
+    doNotFollow: 'node_modules',
+    exclude: {
+      path:
+        '(stories|dist|coverage|__[a-z]+__|_fixtures|cypress|_jest|_nyc|_scripts|scripts|tmp|_templates|.storybook|.testbox|ci|docs)',
+    },
+    reporterOptions: {
+      metrics: {
+        hideModules: false, // hides the modules from the metrics reporter output
+        hideFolders: false, // would hide folders from the metrics reporter output
+        orderBy: 'name', // possible values: name, moduleCount, afferentCouplings, efferentCouplings, instability
+      },
+    },
+  },
+}
