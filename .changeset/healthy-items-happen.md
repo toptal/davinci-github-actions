04317df260398e4268bbf8af8bca2c5e2eed23af
Lasha <72510037+LashaJini@users.noreply.github.com>
2023-02-06 12:37:49 +0400

    fix: contribution action (#103)

---
+++ .changeset/healthy-items-happen.md
@@ -0,0 +1,7 @@
+---
+'davinci-github-actions': patch
+---
+
+### notify-jira-about-contribution
+
+- escape single quotes in PR's title
---
+++ notify-jira-about-contribution/notify-jira.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+generate_data()
+{
+  cat <<EOF
+  {
+    "data": "{
+      "author": "$AUTHOR",
+      "author_url": "$AUTHOR_URL",
+      "title": "$TITLE",
+      "number": "$NUMBER",
+      "url": "$URL",
+      "repo": "$REPO"
+    }"
+  }
+EOF
+}
+
+generate_data | sed "s/'/\\\\'/g" | curl -H 'Content-type: application/json' --data-binary @- "$JIRA_HOOK"
