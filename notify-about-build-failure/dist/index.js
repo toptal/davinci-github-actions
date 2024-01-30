6a21570e4fffef2dd668a4857ec9d049b272c4ae
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2024-01-30 19:00:04 +0300

    Update GH Action

--- notify-about-build-failure/dist/index.js
+++ notify-about-build-failure/dist/index.js
@@ -2823,8 +2823,9 @@ const https = __nccwpck_require__(687)
 // const slackBotToken = core.getInput('slack-bot-token')
 const topTeamApiKey = core.getInput('top-team-api-key')
 // const slackChannelName = core.getInput('slack-channel-name')
+const fallbackSlackTeamName = core.getInput('fallback-slack-team-name')
 const githubCommitAuthorName = core.getInput('github-commit-author-name')
-// const githubActionRunUrl = core.getInput('github-action-run-url')
+const githubActionRunUrl = core.getInput('github-action-run-url')
 
 // Configuration for TopTeam API access
 const TOP_TEAM_API_OPTIONS = {
@@ -2848,11 +2849,11 @@ const TOP_TEAM_API_OPTIONS = {
 //   },
 // }
 
-// const getSlackMessage = (
-//   slackHandle,
-//   githubActionRunUrl
-// ) => `Hello <@${slackHandle}>!
-// Build ${githubActionRunUrl} is currently failing on master, please fix it to unblock the release candidate or let the proper team know.`
+const getSlackMessage = (
+  slackHandle,
+  githubActionRunUrl
+) => `Hello <@${slackHandle}>!
+Build ${githubActionRunUrl} is currently failing on master, please fix it to unblock the release candidate or let the proper team know.`
 
 // const sendSlackMessage = body => {
 //   const slackRequest = https.request(SLACK_API_OPTIONS, slackResponse => {
@@ -2898,7 +2899,6 @@ const communicationChannelsBody = JSON.stringify({
 const getCommunicationChannelsRequest = https.request(
   TOP_TEAM_API_OPTIONS,
   res => {
-    let isNotified = false
     let result = ''
 
     res.setEncoding('utf8')
@@ -2917,50 +2917,40 @@ const getCommunicationChannelsRequest = https.request(
       }
 
       const teams = parsedResult.data.orgUnits.nodes
-      console.log('teams: ', teams)
-      teams.some(team => {
-        team.directRoles.some(({ member }) => {
-          const github = member.communicationChannels.find(
-            channel =>
-              channel.kind === 'GITHUB' &&
-              channel.value === githubCommitAuthorName
-          )
+      let slackIdentifier = fallbackSlackTeamName
 
-          if (github) {
-            const slack = member.communicationChannels.find(
-              channel => channel.kind === 'SLACK'
-            )
+      const member = teams
+        .flatMap(team => team.directRoles.map(role => role.member))
+        .find(member => {
+          return member.communicationChannels.some(channel => channel.kind === 'GITHUB' && channel.value === githubCommitAuthorName)
+        });
 
-            // const message = getSlackMessage(slack.value, githubActionRunUrl)
+      if (member) {
+        const slack = member.communicationChannels.find(
+          channel => channel.kind === 'SLACK'
+        )
 
-            // const privateMessageChannelId = new URLSearchParams(slack.link).get(
-            //   'id'
-            // )
+        slackIdentifier = slack.value
+        const message = getSlackMessage(slack.value, githubActionRunUrl)
+        const privateMessageChannelId = new URLSearchParams(slack.link).get('id')
 
-            console.log('slack: ', slack)
+        console.log('message: ', message)
+        console.log('privateMessageChannelId: ', privateMessageChannelId)
 
-            // sendSlackMessage({
-            //   text: message,
-            //   channel: privateMessageChannelId,
-            // })
+        // sendSlackMessage({
+        //   text: message,
+        //   channel: privateMessageChannelId,
+        // })
+      }
 
-            // sendSlackMessage({
-            //   text: message,
-            //   channel: slackChannelName,
-            // })
-
-            isNotified = true
-          }
-
-          return isNotified
-        })
-
-        return isNotified
-      })
-
-      if (!isNotified) {
-        const errorMessage = `Unable to notify commit author ${githubCommitAuthorName}`
-        throw new Error(errorMessage)
+      if (slackIdentifier) {
+        console.log('message: ', getSlackMessage(slackIdentifier, githubActionRunUrl))
+        // sendSlackMessage({
+        //   text: getSlackMessage(slackIdentifier, githubActionRunUrl),
+        //   channel: slackChannelName,
+        // })
+      } else {
+        throw new Error('No slack identifier found')
       }
     })
   }
--- notify-about-build-failure/index.js
+++ notify-about-build-failure/index.js
@@ -4,8 +4,9 @@ const https = require('https')
 // const slackBotToken = core.getInput('slack-bot-token')
 const topTeamApiKey = core.getInput('top-team-api-key')
 // const slackChannelName = core.getInput('slack-channel-name')
+const fallbackSlackTeamName = core.getInput('fallback-slack-team-name')
 const githubCommitAuthorName = core.getInput('github-commit-author-name')
-// const githubActionRunUrl = core.getInput('github-action-run-url')
+const githubActionRunUrl = core.getInput('github-action-run-url')
 
 // Configuration for TopTeam API access
 const TOP_TEAM_API_OPTIONS = {
@@ -29,11 +30,11 @@ const TOP_TEAM_API_OPTIONS = {
 //   },
 // }
 
-// const getSlackMessage = (
-//   slackHandle,
-//   githubActionRunUrl
-// ) => `Hello <@${slackHandle}>!
-// Build ${githubActionRunUrl} is currently failing on master, please fix it to unblock the release candidate or let the proper team know.`
+const getSlackMessage = (
+  slackHandle,
+  githubActionRunUrl
+) => `Hello <@${slackHandle}>!
+Build ${githubActionRunUrl} is currently failing on master, please fix it to unblock the release candidate or let the proper team know.`
 
 // const sendSlackMessage = body => {
 //   const slackRequest = https.request(SLACK_API_OPTIONS, slackResponse => {
@@ -79,7 +80,6 @@ const communicationChannelsBody = JSON.stringify({
 const getCommunicationChannelsRequest = https.request(
   TOP_TEAM_API_OPTIONS,
   res => {
-    let isNotified = false
     let result = ''
 
     res.setEncoding('utf8')
@@ -98,50 +98,49 @@ const getCommunicationChannelsRequest = https.request(
       }
 
       const teams = parsedResult.data.orgUnits.nodes
-      console.log('teams: ', teams)
-      teams.some(team => {
-        team.directRoles.some(({ member }) => {
-          const github = member.communicationChannels.find(
+      let slackIdentifier = fallbackSlackTeamName
+
+      const member = teams
+        .flatMap(team => team.directRoles.map(role => role.member))
+        .find(member => {
+          return member.communicationChannels.some(
             channel =>
               channel.kind === 'GITHUB' &&
               channel.value === githubCommitAuthorName
           )
-
-          if (github) {
-            const slack = member.communicationChannels.find(
-              channel => channel.kind === 'SLACK'
-            )
-
-            // const message = getSlackMessage(slack.value, githubActionRunUrl)
-
-            // const privateMessageChannelId = new URLSearchParams(slack.link).get(
-            //   'id'
-            // )
-
-            console.log('slack: ', slack)
-
-            // sendSlackMessage({
-            //   text: message,
-            //   channel: privateMessageChannelId,
-            // })
-
-            // sendSlackMessage({
-            //   text: message,
-            //   channel: slackChannelName,
-            // })
-
-            isNotified = true
-          }
-
-          return isNotified
         })
 
-        return isNotified
-      })
+      if (member) {
+        const slack = member.communicationChannels.find(
+          channel => channel.kind === 'SLACK'
+        )
 
-      if (!isNotified) {
-        const errorMessage = `Unable to notify commit author ${githubCommitAuthorName}`
-        throw new Error(errorMessage)
+        slackIdentifier = slack.value
+        const message = getSlackMessage(slack.value, githubActionRunUrl)
+        const privateMessageChannelId = new URLSearchParams(slack.link).get(
+          'id'
+        )
+
+        console.log('message: ', message)
+        console.log('privateMessageChannelId: ', privateMessageChannelId)
+
+        // sendSlackMessage({
+        //   text: message,
+        //   channel: privateMessageChannelId,
+        // })
+      }
+
+      if (slackIdentifier) {
+        console.log(
+          'message: ',
+          getSlackMessage(slackIdentifier, githubActionRunUrl)
+        )
+        // sendSlackMessage({
+        //   text: getSlackMessage(slackIdentifier, githubActionRunUrl),
+        //   channel: slackChannelName,
+        // })
+      } else {
+        throw new Error('No slack identifier found')
       }
     })
   }
